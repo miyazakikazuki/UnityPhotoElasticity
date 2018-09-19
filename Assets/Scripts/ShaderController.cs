d751b37f6b51aae0081b14605e1469b7b09e241a
Kazuki Miyazaki <kazuki.saxophone@gmail.com>
2018-09-19 17:11:36 +0900

    InitialCommit

---
+++ Assets/Scripts/ShaderController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShaderController : MonoBehaviour
+{
+	void Start()
+	{
+		GetComponent<Renderer>().material.SetColor("_BaseColor", Color.black);
+	}
+}
